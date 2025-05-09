a0ad6b129549b18f891b0afdadaad39bd988a412
Carlos Eduardo Maximino <cmax15@outlook.com.br>
2025-05-09 09:30:14 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,ts,tsx}", // Certifique-se de que os arquivos TSX/JSX estejam inclusos aqui
+    "./src/**/*.{html,js,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: '#ff5722', // Exemplo de cor primária
         secondary: '#4caf50', // Exemplo de cor secundária
+        pink: {
+          200: '#fbb6ce', // Cor rosa
+        },
+        blue: {
+          200: '#93c5fd', // Cor azul
+        },
       },
     },
   },
