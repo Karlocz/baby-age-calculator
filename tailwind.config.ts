8c5903b24e58db1aba573ffa9a1d5c0769a2a7a0
Carlos Eduardo Maximino <cmax15@outlook.com.br>
2025-05-09 14:05:37 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,22 @@ module.exports = {
         secondary: '#4caf50', // Exemplo de cor secundária
         pink: {
           200: '#fbb6ce', // Cor rosa
+          400: '#f06292', // Cor rosa mais forte (pode ser usada em bordas, ícones, etc)
         },
         blue: {
-          200: '#93c5fd', // Cor azul
+          200: '#93c5fd', // Cor azul clara
+          400: '#3b82f6', // Cor azul mais forte
         },
+        green: {
+          100: '#d1fae5', // Cor verde clara
+          400: '#34d399', // Cor verde mais forte
+        },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
     },
   },
+  darkMode: 'class', // Habilitando o modo escuro via classe
   plugins: [],
 }
