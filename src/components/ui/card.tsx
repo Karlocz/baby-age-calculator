96aaf7f713c50b499fd0a36e4290e2499c05d891
Carlos Eduardo Maximino <cmax15@outlook.com.br>
2025-05-09 09:04:27 -0300

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,14 +1,14 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
+import * as React from "react"
+import { cn } from "@/lib/utils"
 
-export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={cn("bg-white rounded-2xl shadow p-6", className)} {...props} />
-  );
+    <div className={cn("rounded-2xl border bg-white shadow p-4", className)} {...props} />
+  )
 }
 
-export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div className={cn("p-2", className)} {...props} />
-  );
+function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("pt-2", className)} {...props} />
 }
+
+export { Card, CardContent }
